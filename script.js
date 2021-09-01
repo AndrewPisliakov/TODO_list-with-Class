3fd4f44bfe9e0ee2dac3710716a85ee38997cf9d
Andrew Pisliakov <pislakov@gmail.com>
2021-09-01 10:52:12 +0300

    added class View

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ class Todo {
                 } */
     }
 
-    add(text) {
+    addItem(text) {
         if (this._readonly == true) {
             alert('Список только для чтения. Добавление, изменение ЗАПРЕЩЕНО');
             console.log(this._data);
@@ -42,10 +42,12 @@ class Todo {
             readonly: false
         });
 
+        this._triggerEvent('change');
+
         console.log(this._data);
     }
 
-    changeText(id, text) {
+    changeItemText(id, text) {
         if (this._readonly == true) {
             alert('Список только для чтения. Изменение ЗАПРЕЩЕНО');
             console.log(this._data);
@@ -62,10 +64,12 @@ class Todo {
 
         result.text = text;
 
+        this._triggerEvent('change');
+
         console.log(this._data);
     }
 
-    delete(id) {   //  ГОТОВО
+    deleteItem(id) {   //  ГОТОВО
         if (this._readonly == true) {
             alert('Список только для чтения. Удаление ЗАПРЕЩЕНО');
             console.log(this._data);
@@ -86,11 +90,14 @@ class Todo {
         let numIndex = this._data.findIndex(elem => elem.id == id);
 
         this._data.splice(numIndex, 1);
+
+        this._triggerEvent('change');
+
         console.log(this._data);
 
     }
 
-    deleteAll() {   //--ГОТОВО--
+    clear() {   //--ГОТОВО--
         if (this._readonly == true) {
             alert('Список только для чтения. Добавление, изменение ЗАПРЕЩЕНО');
             console.log(this._data);
@@ -106,10 +113,13 @@ class Todo {
         }
 
         this._data = [];
+
+        this._triggerEvent('change');
+
         console.log(this._data);
     }
 
-    getAll() {   //      --ГОТОВО--
+    getAllItems() {   //      --ГОТОВО--
         return JSON.parse(JSON.stringify(this._data));
     }
 
@@ -118,8 +128,10 @@ class Todo {
     }
 
     makeReadonly() { // только для чтения       --ГОТОВО--
-        this._readonly == true;
-        alert('Список только для чтения');
+        this._readonly = true;
+
+        this._triggerEvent('change');
+
         console.log(this._data);
     }
 
@@ -128,7 +140,8 @@ class Todo {
 
         if (result) {
             result.readonly = true;
-            alert('Перезапись объекта будет недоступна');
+
+            this._triggerEvent('change');
         }
 
         console.log(this._data);
@@ -136,8 +149,8 @@ class Todo {
 
     makeEditable() {
         this._readonly = false;
-        alert('Весь список доступен к редактированию');
-        console.log(this._data);
+
+        this._triggerEvent('change');
     }
 
     makeItemEditable(id) { //  ГОТОВО
@@ -145,8 +158,11 @@ class Todo {
 
         if (result) {
             result.readonly = false;
-            alert('Объект доступен для изменения.');
+
+            this._triggerEvent('change');
         }
+
+        
 
         console.log(this._data);
     }
@@ -155,7 +171,9 @@ class Todo {
         let result = this._data.find(elem => elem.id == id);
 
         result.completed = true;
-        alert('Это дело выполнено!');
+       
+        this._triggerEvent('change');
+
         console.log(this._data);
     }
 
@@ -163,8 +181,22 @@ class Todo {
         let result = this._data.find(elem => elem.id == id);
 
         result.completed = false;
-        alert('Это дело еще не выполнено');
+        
+        this._triggerEvent('change');
+
         console.log(this._data);
+    }
+
+    addEventListener(eventName, func) {
+        window.addEventListener(`model: ${eventName}`, func);
+    }
+
+    removeEventListener(eventName, func) {
+        window.removeEventListener(`model: ${eventName}`, func);
+    }
+
+    _triggerEvent(eventName) {                                      // прописали свое событие
+        window.dispatchEvent(new Event(`model: ${eventName}`))
     }
 
     _checkStatus(id) {
@@ -172,18 +204,21 @@ class Todo {
         console.log(result);
     }
 
+
 }
 
 
 class Storage {
     constructor(key) {
         this._key = key;
-    }
 
-    setData(data) {
         if (typeof this._key !== "string") {
             this._key = this._key.toString();
         }
+    }
+
+    setData(data) {
+
         localStorage.setItem(this._key, JSON.stringify(data));
     }
 
@@ -193,6 +228,74 @@ class Storage {
 };
 
 
+
+class View {
+    constructor(todo) {
+        this._todo = todo;
+        this._htmlElement = document.createElement('div');
+        this._htmlElement.id = Date.now();
+
+        this._render = this._render.bind(this);
+
+    }
+
+    /*     view {
+            _todo : {
+                []
+            },
+            _htmlElement : ...;
+            _htmlElement.id: ..;,
+    
+            initialize()
+        } */
+
+    initialize() {
+        this._todo.addEventListener('change', this._render);    // каждый раз получаем уведомление //todo.addEventListener();
+        document.body.append(this._htmlElement);            // initialize() вызывается один раз и поэтому пишем его сюда. 
+        
+        this._render();
+    }
+
+    destroy() {
+        this._todo.removeEventListener('change', this._render);
+        this._htmlElement.remove();
+    }
+
+
+    _render() {                                                  // отрисовать, представить    
+        let todoItems = this._todo.getAllItems();                // получаем актуальное состояние элементов
+        let todoItemsReadonly = this._todo.isReadonly();         // читаем наше поле для изменения
+
+        let html = '';
+
+        html += `
+        <div class="container">
+            <div class="header">                                          
+                <input type="text" ${todoItemsReadonly ? 'disabled' : ''} id="tagInput_${this._htmlElement.id}" placeholder="Your tagName"> 
+                <button id="tagAdd_${this._htmlElement.id}" ${todoItemsReadonly ? 'disabled' : ''}>+</button>
+            </div>
+            <div id="tags">
+                <div id="notification_${this._htmlElement.id}">Tag list</div>
+                <ul id="tagsList_${this._htmlElement.id}" class="tagList">`             /* добавить классы!!! и SCC */
+        for (let todoItem of todoItems) {
+            html += `<li> ${todoItem.text} <button class="close-dagger delete" ${todoItemsReadonly || todoItem.readonly ? 'disabled' : ''}>&#10006</button></li>`
+        }
+        html += `</ul> 
+            </div>
+        </div>
+        `
+        this._htmlElement.innerHTML = html;
+
+        this._subscribeOnUserAction();
+    }
+
+    _subscribeOnUserAction() {}  // сюда внести все слушатели событий кнопок инпутов ... 
+
+};
+
+let tagInput = document.querySelector('#tagInput');
+let addButton = document.querySelector('#tagAdd');
+let tagsList = document.querySelector('#tagsList');
 
 
 
